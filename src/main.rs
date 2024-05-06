f7e1abdd0623351966d18c93bfacc16acca243f7
watthedoodle <watthedoodle@gmail.com>
2024-05-06 21:52:29 +0100

    heh initial logos rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+use std::io::Write;
+mod distros;
+
+// original bash neofetch source: https://github.com/dylanaraps/neofetch/blob/master/neofetch
+
 fn main() {
-    println!("Hello, world!");
+    let _ = io::stdout().write_all(&format!("{}", distros::arch()).as_bytes());
+}
+
+fn reset() -> &'static str {
+    "\x1B[0m"
 }

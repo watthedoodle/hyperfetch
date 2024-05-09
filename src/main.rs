1ac615f6569a3274bfa5eee1181e2bf9da2c3668
watthedoodle <watthedoodle@gmail.com>
2024-05-09 16:28:32 +0100

    basic infos rendering kinda correct place now

--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,115 @@ mod logos;
 mod os;
 
 // original bash neofetch source: https://github.com/dylanaraps/neofetch/blob/master/neofetch
+// ansci cursor codes https://notes.burke.libbey.me/ansi-escape-codes/
 fn main() {
-    println!(
-        "------------------------------------------------------------------------------------"
-    );
-
     let machine = os::detect();
-    println!("OS detect output -> {:?}", machine);
+    let kernel = kernel::uname();
+    let w = logos::arch().width;
+    let h = logos::arch().height;
+    let ascii = logos::arch().ascii;
 
-    println!(
-        "------------------------------------------------------------------------------------"
-    );
+    self::render(ascii);
+    self::render(self::move_cursor_up(h));
+    self::newline_with_width(w);
+    self::render(self::bold_on());
+    self::render(format!("watthedoodle@workstation"));
+    self::newline_with_width(w);
+    self::render(self::reset());
+    self::render(format!("-----------------"));
+    self::newline_with_width(w);
+    self::render(self::bold_on());
+    self::render(self::info("OS", "???"));
+    self::newline_with_width(w);
+    self::render(self::info("HOST", "???"));
+    self::newline_with_width(w);
+    self::render(self::info("Kernel", &kernel.release));
+    self::newline_with_width(w);
+    self::render(self::info("Uptime", "???"));
+    self::newline_with_width(w);
+    self::render(self::info("Packages", "???"));
+    self::newline_with_width(w);
+    self::render(self::info("Shell", "???"));
+    self::newline_with_width(w);
+    self::render(self::info("Resolution", "???"));
+    self::newline_with_width(w);
+    self::render(format!("DE: <TODO>"));
+    self::newline_with_width(w);
+    self::render(format!("WM: <TODO>"));
+    self::newline_with_width(w);
+    self::render(format!("WM Theme: <TODO>"));
+    self::newline_with_width(w);
+    self::render(format!("Theme: <TODO>"));
+    self::newline_with_width(w);
+    self::render(format!("Icons: <TODO>"));
+    self::newline_with_width(w);
+    self::render(format!("Terminal: <TODO>"));
+    self::newline_with_width(w);
+    self::render(format!("CPU: <TODO>"));
+    self::newline_with_width(w);
+    self::render(format!("GPU: <TODO>"));
+    self::newline_with_width(w);
+    self::render(format!("Memory: <TODO>"));
+    
+    if h > 18 {
+        let excess = h - 18;
+        for num in 0..excess {
+            println!("");
+        }
+        self::print_end();
+    } else {
+        println!("");
+        self::print_end();
+    }
+}
 
-    // let mut logo = String::new();
+fn red() -> String {
+    format!("\x1B[31m")
+}
 
-    // if os.version.to_lowercase().contains("ubuntu") {
-    //     logo = logos::ubuntu();
-    // }
-    // if os.version.to_lowercase().contains("arch") {
-    //     logo = logos::ubuntu();
-    // }
-    // if os.version.to_lowercase().contains("garuda") {
-    //     logo = logos::garuda();
-    // }
-    let _ = io::stdout().write_all(&format!("{}", logos::arch().ascii).as_bytes());
+fn render(x: String) {
+    let _ = io::stdout().write_all(&format!("{}", x).as_bytes());
+}
 
+fn print_end() {
+    println!("");
     println!("");
 }
 
-fn reset() -> &'static str {
-    "\x1B[0m"
+fn newline_with_width(x: u16) {
+    self::render(self::move_cursor_down(1));
+    self::render(self::move_cursor_back(9999));
+    self::render(self::move_cursor_forward(x));
+}
+
+fn move_cursor_up(x: u16) -> String {
+    format!("\x1B[{}A", x)
+}
+
+fn move_cursor_down(x: u16) -> String {
+    format!("\x1B[{}B", x)
+}
+
+fn move_cursor_forward(x: u16) -> String {
+    format!("\x1B[{}C", x)
+}
+
+fn move_cursor_back(x: u16) -> String {
+    format!("\x1B[{}D", x)
+}
+
+fn move_cursor_to(x: u16, y: u16) -> String {
+    format!("\x1B[{};{}H", x, y)
+}
+
+fn bold_on() -> String {
+    format!("\x1B[1m")
+}
+
+fn reset() -> String {
+    format!("\x1B[0m")
+}
+
+fn info(k: &str, v: &str) -> String {
+    format!("{}{}{}{}:{} {}", self::reset(), self::bold_on(), self::red(), k, self::reset(), v)
 }
